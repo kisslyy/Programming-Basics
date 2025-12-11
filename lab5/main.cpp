0afe26e680caf29e0ca1cfa5e530afd6884b1d9a
solonyyy <115837908+kisslyy@users.noreply.github.com>
2025-12-11 12:06:50 +0200

    roughly finished lab5

--- lab5/main.cpp
+++ lab5/main.cpp
@@ -1,125 +1,113 @@
 #include <iostream>
-#include <algorithm>
-
-//task 1
-bool hasTwoSameNumbers(int* a, int n) {
-    for (int i = 0; i < n; ++i)
-        for (int j = i + 1; j < n; ++j)
-            if (a[i] == a[j])
-                return true;
-    return false;
-}
-
-int lastDigit(int x) {
-    return x % 10;
-}
 
 void task1() {
-    const int nMax = 10000;
     int n;
     std::cin >> n;
-    int* a = new int[nMax];
+    int a[10000];
 
-    for (int i = 0; i < n; ++i)
-        std::cin >> a[i];
+    for (int i = 0; i < n; i++) std::cin >> a[i];
 
-    if (hasTwoSameNumbers(a, n)) {
-        std::sort(a, a + n, [](int x, int y) {
-            return lastDigit(x) > lastDigit(y);
-            });
+    bool hasEqual = false;
+    for (int i = 0; i < n && !hasEqual; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (a[i] == a[j]) {
+                hasEqual = true;
+                break;
+            }
+        }
     }
 
-    for (int i = 0; i < n; ++i)
-        std::cout << a[i] << " ";
-    std::cout << std::endl;
+    if (hasEqual) {
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (a[j] % 10 < a[j + 1] % 10) {
+                    int t = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = t;
+                }
+            }
+        }
+    }
 
-    delete[] a;
+    for (int i = 0; i < n; i++) std::cout << a[i] << " ";
 }
 
-//task 2
 int firstDigit(int x) {
-    while (x >= 10)
-        x /= 10;
+    while (x >= 10) x /= 10;
     return x;
 }
 
 int productDigits(int x) {
     int p = 1;
     while (x > 0) {
-        p *= x % 10;
+        p *= (x % 10);
         x /= 10;
     }
     return p;
 }
 
 void task2() {
-    const int nMax = 1000;
     int n;
     std::cin >> n;
-    int* a = new int[nMax];
+    int a[1000];
 
-    for (int i = 0; i < n; ++i)
-        std::cin >> a[i];
+    for (int i = 0; i < n; i++) std::cin >> a[i];
 
-    std::sort(a, a + n, [](int x, int y) {
-        int f1 = firstDigit(x), f2 = firstDigit(y);
-        if (f1 != f2) return f1 < f2;
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - 1 - i; j++) {
 
-        int p1 = productDigits(x), p2 = productDigits(y);
-        if (p1 != p2) return p1 < p2;
+            int fd1 = firstDigit(a[j]);
+            int fd2 = firstDigit(a[j+1]);
 
-        return x < y;
-        });
+            if (fd1 > fd2 ||
+               (fd1 == fd2 && productDigits(a[j]) > productDigits(a[j+1])) ||
+               (fd1 == fd2 && productDigits(a[j]) == productDigits(a[j+1]) && a[j] > a[j+1])) {
 
-    for (int i = 0; i < n; ++i)
-        std::cout << a[i] << " ";
-    std::cout << std::endl;
+                int t = a[j];
+                a[j] = a[j+1];
+                a[j+1] = t;
+            }
+        }
+    }
 
-    delete[] a;
+    for (int i = 0; i < n; i++) std::cout << a[i] << " ";
 }
 
-//task 3
 void task3() {
-    const int nMax = 100, mMax = 100;
     int n, m;
     std::cin >> n >> m;
-    int** A = new int* [n];
-    for (int i = 0; i < n; ++i)
-        A[i] = new int[m];
 
-    for (int i = 0; i < n; ++i)
-        for (int j = 0; j < m; ++j)
-            std::cin >> A[i][j];
+    int a[100][100];
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            std::cin >> a[i][j];
 
+    long long minSum = 0;
+    for (int i = 0; i < n; i++) minSum += a[i][0];
     int minCol = 0;
-    int minSum = 1000000000;
-    for (int j = 0; j < m; ++j) {
-        int sum = 0;
-        for (int i = 0; i < n; ++i)
-            sum += A[i][j];
-        if (sum < minSum) {
-            minSum = sum;
+
+    for (int j = 1; j < m; j++) {
+        long long s = 0;
+        for (int i = 0; i < n; i++) s += a[i][j];
+        if (s < minSum) {
+            minSum = s;
             minCol = j;
         }
     }
 
-    for (int i = 0; i < n; ++i)
-        A[i][minCol] += 3;
+    for (int i = 0; i < n; i++) a[i][minCol] += 3;
 
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < m; ++j)
-            std::cout << A[i][j] << " ";
-        std::cout << std::endl;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++)
+            std::cout << a[i][j] << " ";
+        std::cout << "\n";
     }
-
-    for (int i = 0; i < n; ++i)
-        delete[] A[i];
-    delete[] A;
 }
 
-//task 4
 bool allDigitsDifferent(int x) {
-    bool used[10] = { false };
+    bool used[10];
+    for (int i = 0; i < 10; i++) used[i] = false;
+
     while (x > 0) {
         int d = x % 10;
         if (used[d]) return false;
@@ -130,47 +118,52 @@ bool allDigitsDifferent(int x) {
 }
 
 void task4() {
-    const int nMax = 10000;
     int n;
     std::cin >> n;
-    int* a = new int[nMax];
+    int a[10000];
 
-    for (int i = 0; i < n; ++i)
-        std::cin >> a[i];
+    for (int i = 0; i < n; i++) std::cin >> a[i];
 
-    int newN = 0;
-    for (int i = 0; i < n; ++i)
-        if (!allDigitsDifferent(a[i]))
-            a[newN++] = a[i];
+    int k = 0;
+    for (int i = 0; i < n; i++) {
+        if (!allDigitsDifferent(a[i])) {
+            a[k++] = a[i];
+        }
+    }
+    n = k;
 
-    int finalN = newN;
-    for (int i = 0; i < newN; ++i) {
-        int p = productDigits(a[i]);
-        if (p % 14 == 0 && finalN < nMax)
-            a[finalN++] = a[i];
+    for (int i = 0; i < n; i++) {
+        if (productDigits(a[i]) % 14 == 0) {
+
+            for (int j = n; j > i; j--) {
+                a[j] = a[j-1];
+            }
+            a[i+1] = a[i];
+            n++;
+            i++;
+        }
     }
 
-    for (int i = 0; i < finalN; ++i)
-        std::cout << a[i] << " ";
-    std::cout << std::endl;
-
-    delete[] a;
+    for (int i = 0; i < n; i++) std::cout << a[i] << " ";
 }
 
-//main
+
 int main() {
 
-    while (true) {
+    while (true)
+    {
+        std::cout<<"\nChoose task: ";
 
         int choice;
-        std::cout << "Choose task: ";
         std::cin >> choice;
 
-        switch (choice) {
+        switch (choice)
+        {
         case 1: task1(); break;
         case 2: task2(); break;
         case 3: task3(); break;
         case 4: task4(); break;
+        default: std::cout << "Wrong task number"; break;
         }
     }
 
